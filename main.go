9898392f07e986824b14e9dbf431e1adbf9d491f
M. Ilham Surya Pratama <ilhamsurya26@gmail.com>
2023-01-21 13:58:25 +0700

    Update main.go (#5)

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello there, I'm M. Ilham Surya Pratama")
+		fmt.Fprintf(w, "Hello there, I'm M. Ilham Surya Pratama. I'm Software Engineer")
 	})
 
 	http.ListenAndServe(":3000", nil)
