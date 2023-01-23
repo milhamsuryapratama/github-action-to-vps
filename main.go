f6eb79d87ff9d263b8f2ce2c2cfc9c4061aa5913
milhamsuryapratama <ilham.pratama@ruangguru.com>
2023-01-23 13:44:29 +0700

    test main.go changes

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello there, I'm M. Ilham Surya Pratama. I'm Software Engineer (Backend) at Ruangguru")
+		fmt.Fprintf(w, "Hello there, I'm M. Ilham Surya Pratama. I'm Software Engineer (Backend) at Ruangguru and Tukang Sapu at Gatherloop")
 	})
 
 	http.ListenAndServe(":3000", nil)
